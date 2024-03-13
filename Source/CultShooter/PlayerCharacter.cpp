8860cdcbf0f30274f339539c516d33b52b0bf602
Mjangel1 <269406@stud.inn.no>
2024-03-13 15:56:56 +0100

    Player Movement
    Basic Movement
    with sprint

---
+++ Source/CultShooter/PlayerCharacter.cpp
@@ -0,0 +1,146 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include  "Camera/CameraComponent.h"
+#include "InputCoreTypes.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/SceneComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetMesh()->DestroyComponent();
+	GetMesh()->SetActive(false);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 600.f, 0.0f);
+	GetCharacterMovement()->bIgnoreBaseRotation = true;
+
+	MeshBody = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BodyMesh"));
+	MeshBody->SetupAttachment(RootComponent);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 300.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = false;
+
+
+	SensitivityX = 1.2f;
+	SensitivityY = 0.8f;
+	WalkingSpeed = 600.f;
+	SprintSpeed = 800.f;
+	IsSprinting = false;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetCharacterMovement()->MaxWalkSpeed = WalkingSpeed;
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(Imc, 0);
+		}
+	}
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	const FString TheFloatStr = FString::SanitizeFloat(GetCharacterMovement()->MaxWalkSpeed);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, *TheFloatStr);
+	}
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInputComponent->BindAction(Wasd, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &APlayerCharacter::Jump);
+
+	EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Triggered, this, &APlayerCharacter::StartRunning);
+	EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopRunning);
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MoveInput = Value.Get<FVector2D>();
+
+
+	if (Controller != nullptr)
+	{
+		const FRotator YawPlayerRotation = FRotator(0, GetControlRotation().Yaw, 0);
+
+		const FVector ForwardBackwards = FRotationMatrix(YawPlayerRotation).GetUnitAxis(EAxis::Y);
+
+		const FVector RightDirection = FRotationMatrix(YawPlayerRotation).GetUnitAxis(EAxis::X);
+
+
+		AddMovementInput(ForwardBackwards, MoveInput.X);
+		AddMovementInput(RightDirection, MoveInput.Y);
+	}
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+
+	const FVector2D LookInput = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookInput.X * SensitivityX);
+
+		AddControllerPitchInput(LookInput.Y * SensitivityY);
+
+	}
+
+}
+
+void APlayerCharacter::Jump()
+{
+	Super::Jump();
+}
+
+void APlayerCharacter::StartRunning()
+{
+
+	IsSprinting = true;
+	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+}
+
+void APlayerCharacter::StopRunning()
+{
+
+	IsSprinting = false;
+	GetCharacterMovement()->MaxWalkSpeed = WalkingSpeed;
+}
+
---
+++ Source/CultShooter/PlayerCharacter.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class CULTSHOOTER_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	class USkeletalMeshComponent* MeshBody;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraMovement")
+	float SensitivityX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraMovement")
+	float SensitivityY;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "InputMapping")
+	class UInputMappingContext* Imc;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	class UInputAction* Wasd;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraMovement")
+	class UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	class UInputAction* JumpAction;
+
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	class UInputAction* RunAction;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	bool IsSprinting;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float WalkingSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed;
+
+
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Jump() override;
+	void StartRunning();
+	void StopRunning();
+};
