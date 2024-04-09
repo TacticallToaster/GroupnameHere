9481407dd3c3b167b36261e528eb24fd49d1f6e3
Mjangel1 <269406@stud.inn.no>
2024-04-10 01:38:33 +0200

    Movement/Shooting
    Dash is implemented. the enabling shoot is there and how the shooting mechanic works, how ever is only comments because it's incomplete. using interact interface but still incomplete

--- Source/CultShooter/PlayerCharacter.cpp
+++ Source/CultShooter/PlayerCharacter.cpp
@@ -7,6 +7,7 @@
 #include "EnhancedInputComponent.h"
 #include "InputMappingContext.h"
 #include "EnhancedInputSubsystems.h"
+#include "Components/BoxComponent.h"
 #include "Components/SceneComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 // Sets default values
@@ -15,8 +16,7 @@ APlayerCharacter::APlayerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	GetMesh()->DestroyComponent();
-	GetMesh()->SetActive(false);
+	
 
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
@@ -25,24 +25,23 @@ APlayerCharacter::APlayerCharacter()
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 600.f, 0.0f);
 	GetCharacterMovement()->bIgnoreBaseRotation = true;
 
-	MeshBody = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BodyMesh"));
-	MeshBody->SetupAttachment(RootComponent);
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->TargetArmLength = 300.f;
 	SpringArm->bUsePawnControlRotation = true;
 
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 30.f;
+
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
 	Camera->bUsePawnControlRotation = false;
 
 
-	SensitivityX = 1.2f;
-	SensitivityY = 0.8f;
-	WalkingSpeed = 600.f;
-	SprintSpeed = 800.f;
-	IsSprinting = false;
+	InteractionArea = CreateDefaultSubobject<UBoxComponent>(TEXT("Interaction Area"));
+	InteractionArea->SetupAttachment(RootComponent);
+	
 }
 
 // Called when the game starts or when spawned
@@ -59,6 +58,12 @@ void APlayerCharacter::BeginPlay()
 			Subsystem->AddMappingContext(Imc, 0);
 		}
 	}
+
+	InteractionArea->OnComponentBeginOverlap.AddDynamic(this, &APlayerCharacter::OnBoxBeginOverlap);
+
+	InteractionArea->OnComponentEndOverlap.AddDynamic(this, &APlayerCharacter::OnBoxEndOverlap);
+
+	
 }
 
 // Called every frame
@@ -70,6 +75,8 @@ void APlayerCharacter::Tick(float DeltaTime)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, *TheFloatStr);
 	}
+
+	//RotatePlayer();
 
 }
 
@@ -89,7 +96,12 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 	EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Triggered, this, &APlayerCharacter::StartRunning);
 	EnhancedInputComponent->BindAction(RunAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopRunning);
+
+	EnhancedInputComponent->BindAction(DashAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Dashing);
+
+	EnhancedInputComponent->BindAction(InteractionAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Interaction);
 }
+
 
 void APlayerCharacter::Move(const FInputActionValue& Value)
 {
@@ -142,5 +154,126 @@ void APlayerCharacter::StopRunning()
 
 	IsSprinting = false;
 	GetCharacterMovement()->MaxWalkSpeed = WalkingSpeed;
+}
+
+void APlayerCharacter::Dashing()
+{
+
+	//if statement check if the player is moving can dash
+	if (GetCharacterMovement()->Velocity != FVector::ZeroVector)
+	{
+
+		GetCharacterMovement()->BrakingFriction = 0.0f;
+		const FVector ForwardDirection = GetActorRotation().Vector();
+		LaunchCharacter(ForwardDirection * DashDistance, true, true);
+	}
+}
+
+void APlayerCharacter::Interaction()
+{
+
+	if (Interact)
+	{
+
+		Interact->Interact();
+
+	}
+
+}
+
+void APlayerCharacter::Shoot()
+{
+	if (EnableShoot)
+	{
+
+		//if (ProjectileSpawn)
+		//{
+		//	bIsShooting = true;
+
+		//	UWorld* World = GetWorld();
+		//	if (World != nullptr)
+		//	{
+		//		//This finds where the player in which way player is looking at.
+		//		const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+		//		const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
+
+		//		//Get the rotation of the socket with the name SocketHeadShoot
+		//		const FRotator SocketRotation = GetMesh()->GetSocketRotation("SocketHeadShoot");
+
+		//		//Get the location of the socket with the name SocketHeadShoot and add the socket rotation plus the vector created earlier
+		//		const FVector SocketLocation = GetMesh()->GetSocketLocation("SocketHeadShoot") + SocketRotation.RotateVector(GunTipOff);
+
+		//		FActorSpawnParameters ActorSpawnParams;
+		//		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+		//		ProjectileFired = GetWorld()->SpawnActor<AProjectileActor>(ProjectileSpawn, SocketLocation, SpawnRotation, ActorSpawnParams);
+
+
+
+		//	}
+
+		//}
+
+	}
+
+}
+
+//void APlayerCharacter::IsShooting()
+//{
+//
+//	FTimerHandle TimerHandlePlayerCamera;
+//
+//	//Delay's time for 5 seconds and calls the function StopShoot after 5 seconds when player stop shooting
+//	GetWorld()->GetTimerManager().SetTimer(TimerHandlePlayerCamera, this, &APlayerCharacter::StopShoot, 5.0f, false);
+//
+//}
+
+//void APlayerCharacter::PlayerRotate()
+//{
+//
+//	if (bIsShooting)
+//	{
+//		//Cast to the player controller
+//		const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+//
+//		if (PlayerController->PlayerCameraManager)
+//		{
+//			//Get the camera rotation
+//			FVector PlayerCameraRotation = PlayerController->PlayerCameraManager->GetCameraRotation().Vector();
+//
+//			//Stop Camera rotation to influence the rotation of the player in the Z axis
+//			PlayerCameraRotation.Z = 0.0f;
+//
+//			// the player rotation is equal to the camera rotation
+//			const FRotator PlayerRotation = PlayerCameraRotation.Rotation();
+//
+//			//Set the player Rotation to the camera rotation
+//			SetActorRotation(PlayerRotation);
+//		}
+//
+//
+//	}
+//
+//}
+
+//void APlayerCharacter::StopShoot()
+//{
+//
+//	bIsShooting = false;
+//
+//}
+
+
+void APlayerCharacter::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                         UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Interact = Cast<IInteractInterface>(OtherActor);
+
+
+}
+
+void APlayerCharacter::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
--- Source/CultShooter/PlayerCharacter.h
+++ Source/CultShooter/PlayerCharacter.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
+#include "InteractInterface.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Components/SkeletalMeshComponent.h"
@@ -33,9 +34,6 @@ public:
 
 public:
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-	class USkeletalMeshComponent* MeshBody;
-
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
 	class UCameraComponent* Camera;
@@ -44,10 +42,10 @@ public:
 	class USpringArmComponent* SpringArm;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraMovement")
-	float SensitivityX;
+	float SensitivityX = 1.2f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CameraMovement")
-	float SensitivityY;
+	float SensitivityY = 0.8f;
 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "InputMapping")
@@ -63,20 +61,40 @@ public:
 	class UInputAction* JumpAction;
 
 
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
 	class UInputAction* RunAction;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	class UInputAction* DashAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interact")
+	class UInputAction* InteractionAction;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	bool IsSprinting;
+	float DashDistance = 2000.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	bool IsSprinting = false;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Movement")
-	float WalkingSpeed;
+	float WalkingSpeed = 600.f;
 
 	UPROPERTY(BlueprintReadOnly, Category = "Movement")
-	float SprintSpeed;
+	float SprintSpeed = 800.f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Box Interaction")
+	class UBoxComponent* InteractionArea;
+
+
+	IInteractInterface* Interact = nullptr;
+
+	bool EnableShoot = false;
+
+
+	/*UPROPERTY()
+	class AProjectileActor* ProjectileFired;*/
+
+	/*bool bIsShooting = false;*/
 
 	UFUNCTION()
 	void Move(const FInputActionValue& Value);
@@ -84,4 +102,22 @@ public:
 	void Jump() override;
 	void StartRunning();
 	void StopRunning();
+	void Dashing();
+	void Interaction();
+	void Shoot();
+	/*void StopShoot();*/
+	/*void IsShooting();
+	void PlayerRotate();*/
+
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shooting thing")
+	TSubclassOf<AActor> ProjectileSpawn;*/
+
+
 };
